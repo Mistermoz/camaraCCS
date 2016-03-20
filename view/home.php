3f535b173187bf633bc55c1c3c80e556624f6eb0
claudio.donoso <claudio.donoso@STGUMW-NMX0009.local>
2016-03-19 23:35:28 -0300

    add home html

---
+++ view/home.php
@@ -0,0 +1,83 @@
+<div class="main">
+	
+	<div class="col-center">
+    
+    	<div class="left">
+            <div class="indicadores">
+            	<p><strong>INDICADORES ECONOMICOS</strong>    <span>UF: 25.350,09</span>   -    <span>Dólar Obs: $715,8</span>   -  <span>Euro: $710,5</span></p>
+            </div>
+            
+            <div class="slider">
+            	<ul class="bxslider-home">
+                	<li>
+                        <img src="img/foto-slide.jpg" />
+                    	<span class="frase1">Negocios internacionales</span>
+                        <span class="frase2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                    </li>
+                    <li>
+                        <img src="img/foto-slide.jpg" />
+                    	<span class="frase1">Negocios internacionales</span>
+                        <span class="frase2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                    </li>
+                    <li>
+                        <img src="img/foto-slide.jpg" />
+                    	<span class="frase1">Negocios internacionales</span>
+                        <span class="frase2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="bnoticias">
+                <div class="tit-news-h">Noticias</div>
+            
+                <div class="box-news box-news1">
+                    <img src="img/foto-news.jpg" />
+                    <span>25 noviembre, 2015</span>
+                    <p>OMC logra acuerdo que beneficia exportaciones chilenas.</p>
+                    <a href="#">Leer más</a>
+                </div>
+                
+                <div class="box-news">
+                    <img src="img/foto-news.jpg" />
+                    <span>25 noviembre, 2015</span>
+                    <p>OMC logra acuerdo que beneficia exportaciones chilenas.</p>
+                    <a href="#">Leer más</a>
+                </div>
+            </div>
+        </div>
+        
+        <div class="box-info">
+            <div class="right">
+            
+                <div class="buscador">
+                    <span>Buscador</span>
+                    <input type="text" class="in" placeholder="Que desea buscar..."/>
+                    <input name="" class="bt" type="button" value="" />
+                </div>
+                
+                <div class="calendario">
+                    <span class="txt">Próxima capacitación “Portal Comex CCS”</span>
+                    <span class="date">13 <br /><span>ENE</span></span>
+                    <a href="#">Ver información y próximas fechas</a>
+                </div>
+                
+                <div class="banner">
+                    <img src="img/home_ingreso_clientes_03.jpg" />
+                    <span class="txt">hazte cliente</span>
+                    <span class="txt02">Lorem ipsum dolor sit amet, consectetur adipiscing elit onvallis sit amet quam vitae feugiat. Lorem ipsum dolor sit amet.</span>
+                    <a href="#">infórmate como aquí</a>
+                </div>
+                
+            </div>
+            
+            <div class="banner01 banner-home"><a href=""><img src="img/banner-home01.jpg" /></a></div>
+            
+            <div class="banner02 banner-home"><a href=""><img src="img/banner-home02.jpg" /></a></div>
+            
+            <div class="banner03 banner-home"><a href=""><img src="img/banner-home03.jpg" /></a></div>
+        </div>
+    	
+
+	</div>
+    
+</div>
