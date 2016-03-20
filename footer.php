25fc0f47a25a11096c3eade864802ac1c41a69ac
claudio.donoso <claudio.donoso@STGUMW-NMX0009.local>
2016-03-19 23:35:14 -0300

    add home responsive

---
+++ footer.php
@@ -0,0 +1,92 @@
+<!--FOOTER-->
+   <div class="footer">
+      
+      
+      <div class="col-center">
+      		
+        <div class="blocks-info">
+            <ul>
+            	<span>BIBLIOTECA</span>
+            	<li><a href="">- &iquest;C&oacute;mo Importar</a></li>
+                <li><a href="">- &iquest;C&oacute;mo Exportar?</a></li>
+                <li><a href="">- Manual Importador y Exportador</a></li>
+                <li><a href="">- Normativa SAG</a></li>
+                <li><a href="">- Exportaci&oacute;n de Servicios </a></li>
+                <li><a href="">- Acuerdos Comerciales</a></li>
+                <li><a href="">- Estudios</a></li>
+                <li><a href="">- Descarga de Arancel Aduanero</a></li>
+            </ul>
+            
+            <ul>
+            	<span>   Servicios online  </span>
+            	<li><a href="">- Comex Plus</a></li>
+                <li><a href="">- Tracking Comex</a></li>
+                <li><a href="">- Reporte Estad&iacute;sticos</a></li>
+                <li><a href="">- Nacionales e Internacionales</a></li>
+            </ul>
+            
+            <ul>
+            	<span>Negocios</span>
+            	<li><a href="">- Export helpdesk</a></li>
+                <li><a href="">- Europa</a></li>
+                <li><a href="">- Misiones Empresariales </a></li>
+                <li><a href="">- Carnet ATA</a></li>
+                <li><a href="">- Tarjeta APEC</a></li>
+                <li><a href="">- Asia Pac&iacute;fico</a></li>
+                <li><a href="">- Certificado de Negocios</a></li>
+                <li><a href="">- Empresariales </a></li>
+                <li><a href="">- Arbitraje Internacional </a></li>
+            </ul>
+            
+            <ul>
+            	<span>Gu&iacute;a SS</span>
+            	<li><a href="">- Visaci&oacute;n de Documentos</a></li>
+                <li><a href="">- Ferias Internacionales </a></li>
+            </ul>
+            
+            <ul>
+            	<span>CAPACITACI&oacute;N</span>
+            	<li><a href="">- Calendario Anual</a></li>
+            </ul>
+        </div>
+            
+        <div class="logo"><img src="img/logofooter.png" /></div>
+            
+            
+      </div>
+      
+      
+      <div class="piebackground">
+      		<div class="col-center">
+            	<div class="phone">
+                	<p><span>(56 2) 2360 7000</span> <br />
+                    <span>(56 2) 2360 7076</span> <br />
+                    <span>(56 2) 2360 7115</span></p>
+                </div>
+                <div class="edificio">
+                	<p><span>Edificio del Comercio <br />
+                    Monjitas 392, 1er piso, Santiago - Chile</span><br class="nobr"/>
+                    Estación de Metro Bellas Artes</p>
+                </div>
+                <div class="hora">
+                	<p>Lunes a Jueves 8:30 a 18:00<br />
+					Viernes de 8:30 a 16:30.</p>
+                </div>
+                <div class="contacto">
+                	<p><a href="mailto:comexccs@ccs.cl" target="_blank">comexccs@ccs.cl</a></p>
+                </div>
+                <div class="politicas">
+                	<h2>Pol&iacute;ticas de Privacidad</h2>
+                    <p>La informaci&oacute;n contenida en este
+                    sitio es de propiedad de la
+                    C&aacute;mara de Comercio de Santiago.</p>
+                </div>
+            </div>
+      </div>
+      
+   </div>
+<!--FOOTER-->
+
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>PortalComex</title>
+
+<link href="style.css" rel="Stylesheet" media="screen" type="text/css" />
+<link href="css/font-awesome.min.css" rel="Stylesheet" media="screen" type="text/css" />
+<link href="css/default.css" rel="Stylesheet" media="screen" type="text/css" />
+<!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'> -->
+
+<link rel="shortcut icon" href="img/favicon.png" />
+<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" />
+<link rel='icon' href='img/favicon.png' type='image/x-icon' />
+
+<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" src="js/jquery.bxslider.js"></script>
+<script type="text/javascript" src="js/funciones.js"></script>
+
+<meta name="description" content="">
+<meta name="keywords" content="">
+
+</head>
+
+<body>
+
+<div class="topoculto">
+	<div class="col-center">
+    	
+        <ul class="listtop">
+        	<li><a href="" class="candado">COMEX ONLINE</a></li>
+            <li><a href="" class="candado">Estadisticas de Formas de Pago </a></li>
+            <li><a href="" class="candado">Calculadora de aranceles</a></li>
+            <li><a href="" class="candado">PRESENTACIÓN DE CHILE</a></li>
+            <li><a href="" class="candado">DIRECTORIO COMEX</a></li>
+        </ul>
+        
+        <div class="colform">
+        	<div class="col">
+            	<label>USUARIO</label>
+            	<input type="text" class="it"/>
+            </div>
+            <div class="col">
+            	<label>CONTRASEÑA</label>
+            	<input type="text" class="it"/>
+            </div>
+             <div class="col">
+            	<input id="envio" type="text" class="bt" value="INGRESAR"/>
+            </div>
+            <a href="#">¿Olvidaste tu contraseña?</a>
+        </div>
+        
+    </div>
+</div>
+
+<div class="header">
+	
+    <div class="col-center">
+    	
+        <div class="nav-top">
+        	<ul>
+            	<li><a href="#">HOME</a></li>
+                <li><a href="#" class="especial">SOLICITAR DEMO</a></li>
+                <li><a href="#" class="ingresoCliente">INGRESAR / HAZTE CLIENTE</a></li>
+                <li><a href="#" class="especial">CONTÁCTENOS</a></li>
+                <li><a href="#">ENGLISH VERSION</a></li>
+            </ul>
+        </div>
+        
+        <div class="logo"><img src="img/logoportal.jpg"/></div>
+        <div class="logoccs">
+            <img src="img/logo-ccs.jpg"/>
+
+            <div class="menu-mobile">
+                <a href="#" class="fa fa-bars"></a>
+            </div>
+        </div>
+        
+        <div class="nav">
+        	<ul>
+            	<li><a href="#">Servicios online</a>
+                	<div class="subnav">
+                    	<ul>
+                        	<li><a href=""><img src="img/bulet10.png" />Comex ONLINE</a>
+                            	<div class="subsubnav">
+                                	<img src="img/fotonav.jpg" />
+                                    <p>01 Lorem ipsum dolor sit amet, consectetur adipiscing elit onvallis sit amet quam vitae feugiat. Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </li>
+                            <li><a href=""><img src="img/bulet10.png" />Calculadora de Aranceles</a>
+                            	<div class="subsubnav">
+                                	<img src="img/fotonav.jpg" />
+                                    <p>02 Lorem ipsum dolor sit amet, consectetur adipiscing elit onvallis sit amet quam vitae feugiat. Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </li>
+                            <li><a href=""><img src="img/bulet10.png" />Presentación de Chile</a>
+                            	<div class="subsubnav">
+                                	<img src="img/fotonav.jpg" />
+                                    <p>03 Lorem ipsum dolor sit amet, consectetur adipiscing elit onvallis sit amet quam vitae feugiat. Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </li>
+                            <li><a href=""><img src="img/bulet11.png" />Estadísticas  Internacionales</a>
+                            	<div class="subsubnav">
+                                	<img src="img/fotonav.jpg" />
+                                    <p>04 Lorem ipsum dolor sit amet, consectetur adipiscing elit onvallis sit amet quam vitae feugiat. Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </li>
+                            <li><a href=""><img src="img/bulet10.png" />Estadísticas de Formas de pago</a>
+                            	<div class="subsubnav">
+                                	<img src="img/fotonav.jpg" />
+                                    <p>05 Lorem ipsum dolor sit amet, consectetur adipiscing elit onvallis sit amet quam vitae feugiat. Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </li>
+                            <li><a href=""><img src="img/bulet10.png" />DIRECTORIO</a>
+                            	<div class="subsubnav">
+                                	<img src="img/fotonav.jpg" />
+                                    <p>06 Lorem ipsum dolor sit amet, consectetur adipiscing elit onvallis sit amet quam vitae feugiat. Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li><a href="#">Expansión Internacional</a>
+                	<div class="subnav">
+                    	<ul>
+                        	<li><a href=""><img src="img/bulet12.png" />CARNET ATA</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />TARJETA APEC</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />MISIONES EMPRESARIALES</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />CERTIFICADO DE NEGOCIOS EMPRESARIALES</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />ferias internacionales</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />exportación de servicios</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />export helpdesk europa</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />contrapartes comerciales asia pacífico</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li><a href="#">BIBLIOTECA</a>
+                	<div class="subnav">
+                    	<ul>
+                        	<li><a href=""><img src="img/bulet12.png" />¿CÓMO IMPORTAR?</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />¿CÓMO EXPORTAR?</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />NORMATIVA SAG</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />ACUERDOS COMERCIALES</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />ESTUDIOS</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />DESCARGA DE ARANCEL ADUANERO</a></li>
+                            <li><a href=""><img src="img/bulet12.png" />INCOTERMS 2010</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li><a href="#">Guía SS</a>
+                	<div class="subnav">
+                    	<ul>
+                        	<li><a href=""><img src="img/bulet12.png" />VISACIÓN DE documentos </a></li>
+                            <li><a href=""><img src="img/bulet12.png" />arbitraje internacional </a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
