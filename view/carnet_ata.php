f79e06e98774ea50db2e6814dcb9512bda054774
claudio.donoso <claudio.donoso@STGUMW-NMX0009.local>
2016-03-21 04:38:04 -0300

    add carnet ata

---
+++ view/carnet_ata.php
@@ -0,0 +1,120 @@
+<div class="main ata">
+	<div class="indicadores top-indicadores">
+		<p><span>UF: 25.350,09</span>   -    <span>Dólar Obs: $715,8</span>   -  <span>Euro: $710,5</span></p>
+	</div>
+		
+	<div class="container">
+		<div class="top-title">
+			<h2 class="title">CARNET ATA</h2>
+
+			<p class="description"><span>Pasaporte de mercancías para salidas temporales a ferias, exposiciones y de material profesional.</span></p>
+		</div>
+
+		<div class="banner-image">
+			<img src="./img/banner_ata.jpg" alt="">
+		</div>
+
+		<div class="content">
+			<div class="breadcrum">
+				<a href="/" class="first">Home</a>
+				<a href="" class="last">Certificado de Negocios Internacionales</a>
+			</div>
+
+			<div class="tabs"> 
+			  <nav role='navigation' class="transformer-tabs">
+			    <ul>
+			      <li><a href="#tab-1" class="active">¿Qué es?</a></li>
+			      <li><a href="#tab-2">¿Para qué se utiliza?</a></li>
+			      <li><a href="#tab-3">¿Cómo obtenerlo?</a></li>
+			      <li><a href="#tab-4">Tarifa</a></li>
+			    </ul>
+			  </nav>
+			  
+			  <div id="tab-1" class="active">
+			    <div class="info-top">	
+				  	<h4>¿Qué es el Carnet ATA?</h4>
+				    
+				    <p>El Carnet ATA es un documento aduanero que permite la libre admisión temporal de mercancías, exenta de derechos de importación y otros impuestos, tales como el IVA en más de 80 países.</p>
+
+				    <p>Resultado de convenios internacionales, el Carnet ATA ha sido utilizado internacionalmente por las empresas por más de 40 años, constituyendo un verdadero “pasaporte” para las mercancías en viajes de negocios o participación en ferias y eventos.</p>
+
+				    <p>La Cámara de Comercio de Santiago (CCS) ha sido designada por el Servicio Nacional de Aduanas como la entidad emisora y garante del Carnet ATA en Chile. La CCS forma parte de una red mundial de entidades ATA, liderada por la Cámara de Comercio Internacional (ICC).</p>
+
+				    <div class="row-info border-one">
+				    	<div class="column-info">
+				    		<h4>Ficha de Inscripción</h4>
+
+				    		<a href="#"><img src="./img/icon_solicitud_ata.jpg" alt=""> Solicitud Emisión ATA</a>
+				    	</div>
+
+				    	<div class="column-info contacto-info">
+				    		<h4>Contacto</h4>
+
+				    		<a href="mailto:comexccs@ccs.cl"><img src="./img/icon-envelope.jpg" alt=""> <span>comexccs@ccs.cl</span></a>
+
+				    		<a href="tel:56223607120" class="telefonos"><img src="./img/icon-phone.jpg" alt=""/> <span>562 2360 7120 - 562 2360 7076 - 562 2360 7115</span></a>
+				    	</div>
+				    </div>
+				  </div>
+
+				  <div class="countries border-one">
+				  	<h4>Listado de paises (71)</h4>
+
+				  	<div class="list-countries">
+				  		<div class="country"><span class="flag-icon flag-icon-de"></span> Alemania</div>
+				  		<div class="country"><span class="flag-icon flag-icon-ad"></span> Andorra</div>
+				  		<div class="country"><span class="flag-icon flag-icon-dz"></span> Argelia</div>
+				  		<div class="country"><span class="flag-icon flag-icon-au"></span> Australia</div>
+				  		<div class="country"><span class="flag-icon flag-icon-at"></span> Austria</div>
+				  		<div class="country"><span class="flag-icon flag-icon-be"></span> Bélgica</div>
+				  		<div class="country"><span class="flag-icon flag-icon-by"></span> Bielorrusia</div>
+				  		<div class="country"><span class="flag-icon flag-icon-ba"></span> Boznia y Herzegovina</div>
+				  	</div>
+
+				  	<p class="small">(*) Bajo la autoridad y tutela de otro estado garante</p>
+				  </div>
+
+				  <div class="row-info bottom-banners">
+				  	<div class="column-info">
+				  		<h4>Entidad Emisora y Garate</h4>
+
+				  		<img src="./img/entidad_emisora_garate.jpg" alt="">
+				  	</div>
+
+				  	<div class="column-info">
+				  		<h4>Con el Apoyo de:</h4>
+
+				  		<img src="./img/ata_apoyo_de.jpg" alt="">
+				  	</div>
+				  </div>
+			  </div>
+			  
+			  <div id="tab-2">
+			    <h4>Tab 2</h4>
+			    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur est natus esse minima nihil quidem tenetur alias pariatur. Obcaecati repudiandae temporibus provident sapiente iure doloribus praesentium voluptates dolores quia eos velit fugit cum ipsam. Deleniti fugiat maxime vel tempore illum esse illo fugit quam recusandae aut aperiam omnis at quaerat!
+			  </div>
+			  
+			  <div id="tab-3">
+			    <h4>Tab 3</h4>
+			    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores numquam cupiditate aliquam quisquam repellendus fugit eaque asperiores voluptatem ut accusamus soluta corporis in facere quae provident itaque magni eum repellat ducimus dolore. Beatae aperiam accusamus at voluptatem ad sunt mollitia perspiciatis tempora numquam rerum aliquam deserunt illum necessitatibus nisi omnis.
+			  </div>
+			  
+			  <div id="tab-4">
+			    <h4>Tab 4</h4>
+			    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste officiis impedit ut culpa quaerat error pariatur voluptatum sapiente alias quasi itaque voluptas expedita necessitatibus odio dolorem totam veniam quisquam nulla vero placeat corporis cupiditate assumenda amet inventore mollitia quidem similique laudantium maxime aperiam ea reprehenderit iusto a ad tempora harum.
+			  </div>
+			</div>
+		</div>
+
+		<div class="bottom-content">
+			<div class="indicadores">
+				<p><strong>INDICADORES ECONOMICOS</strong>    <span>UF: 25.350,09</span>   -    <span>Dólar Obs: $715,8</span>   -  <span>Euro: $710,5</span></p>
+			</div>
+
+			<div class="buscador">
+        <input type="text" class="in" placeholder="Que desea buscar..."/>
+        <input name="" class="bt" type="button" value="" />
+			</div>
+		</div>
+	</div>
+</div>
