d8ddec5e39899e9c67d425c3bddd7ffb16385324
claudio.donoso <claudio.donoso@STGUMW-NMX0009.local>
2016-03-21 20:40:05 -0300

    add formas de pago

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include ("header.php");?>
 
-	<?php include ('view/visaciones.php'); ?>
+	<?php include ('view/formas_de_pago.php'); ?>
     
 <?php include ("footer.php");?>
---
+++ view/formas_de_pago.php
@@ -0,0 +1,248 @@
+<div class="main formas_de_pago">
+	<div class="indicadores top-indicadores">
+		<p><span>UF: 25.350,09</span>   -    <span>Dólar Obs: $715,8</span>   -  <span>Euro: $710,5</span></p>
+	</div>
+		
+	<div class="container">
+		<div class="top-title">
+			<h2 class="title">Formas de Pago</h2>
+
+			<p class="description"><span></span></p>
+		</div>
+
+		<div class="content">
+			<div class="breadcrum">
+				<a href="/" class="first">Home</a>
+				<a href="#">Formas de pago</a>
+				<a href="#" class="last">Consultas por RUT País</a>
+			</div>
+
+			<div class="tabs"> 
+			  <nav role='navigation' class="transformer-tabs">
+			    <ul>
+			      <li><a href="#tab-1">Consulta por RUT Forma de pago</a></li>
+			      <li><a href="#tab-2">Consulta por RUT País</a></li>
+			      <li><a href="#tab-3">Consulta por RUT Glosa</a></li>
+			      <li><a href="#tab-4" class="active">Consulta por detalle País</a></li>
+			    </ul>
+			  </nav>
+			  
+			  <div id="tab-1">
+			    <div class="info-top">	
+				  	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem beatae ex, cumque consequatur. Iusto ut, reprehenderit necessitatibus velit eveniet consectetur officiis maxime placeat, dolor debitis quae nihil. Consequatur, sequi, voluptatem!</p>
+				  </div>
+			  </div>
+			  
+			  <div id="tab-2">
+			    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat at fuga sed explicabo ex minus ad dolor eum distinctio numquam ducimus labore, repudiandae sapiente mollitia qui quia culpa, eaque nam.</p>
+			  </div>
+			  
+			  <div id="tab-3">
+			    <h4>Tab 3</h4>
+			    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores numquam cupiditate aliquam quisquam repellendus fugit eaque asperiores voluptatem ut accusamus soluta corporis in facere quae provident itaque magni eum repellat ducimus dolore. Beatae aperiam accusamus at voluptatem ad sunt mollitia perspiciatis tempora numquam rerum aliquam deserunt illum necessitatibus nisi omnis.
+			  </div>
+
+			  <div id="tab-4" class="active">
+			    <form action="#" id="formulario-consulta-pais">
+						<fieldset>
+							<div class="row-form">
+								<div class="input select">
+									<label>Desde</label>
+									
+									<div class="box-form">
+										<div class="select-box">
+											<select name="danio" required>
+												<option value="">Año</option>
+												<option value="1">2016</option>
+											</select>
+
+											<span class="fa fa-angle-down"></span>
+										</div>
+
+										<div class="select-box">
+											<select name="dmes" required>
+												<option value="">Mes</option>
+												<option value="1">Diciembre</option>
+											</select>
+
+											<span class="fa fa-angle-down"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="input select">
+									<label>Hasta</label>
+									
+									<div class="box-form">
+										<div class="select-box">
+											<select name="hanio" required>
+												<option value="">Año</option>
+												<option value="1">2016</option>
+											</select>
+
+											<span class="fa fa-angle-down"></span>
+										</div>
+
+										<div class="select-box">
+											<select name="hmes" required>
+												<option value="">Mes</option>
+												<option value="1">Diciembre</option>
+											</select>
+
+											<span class="fa fa-angle-down"></span>
+										</div>
+									</div>
+								</div>
+
+								<div class="input select">
+									<label>Tipo</label>
+									
+									<div class="select-box">
+										<select name="tipo" required>
+											<option value="">Tipo</option>
+											<option value="1">Option 1</option>
+										</select>
+
+										<span class="fa fa-angle-down"></span>
+									</div>
+								</div>
+
+								<div class="input select">
+									<label>Formas de pago</label>
+									
+									<div class="select-box">
+										<select name="formas" required>
+											<option value="">Seleccione</option>
+											<option value="1">Todos</option>
+										</select>
+
+										<span class="fa fa-angle-down"></span>
+									</div>
+								</div>
+							</div>
+
+							<div class="row-form">
+								<div class="input select">
+									<label>País de origen</label>
+									
+									<div class="select-box">
+										<select name="origen" required>
+											<option value="">Seleccione</option>
+											<option value="1">Pais 1</option>
+										</select>
+
+										<span class="fa fa-angle-down"></span>
+									</div>
+								</div>
+
+								<div class="input select">
+									<label>País de Adquicición</label>
+									
+									<div class="select-box">
+										<select name="adquicicion" required>
+											<option value="">Seleccione</option>
+											<option value="1">Pais 1</option>
+										</select>
+
+										<span class="fa fa-angle-down"></span>
+									</div>
+								</div>
+
+								<div class="input select">
+									<label>País de Destino</label>
+									
+									<div class="select-box">
+										<select name="destino" required>
+											<option value="">Seleccione</option>
+											<option value="1">Pais 1</option>
+										</select>
+
+										<span class="fa fa-angle-down"></span>
+									</div>
+								</div>
+
+								<div class="input text">
+									<label>RUT:</label>
+
+									<input type="text" name="rut" placeholder="Sin puntos ni guión" id="rut" required>
+								</div>
+							</div>
+
+							<div class="row-form">
+								<div class="input select">
+									<label>Clave Económica</label>
+									
+									<div class="select-box">
+										<select name="clave" required>
+											<option value="">Seleccione</option>
+											<option value="1">Todos</option>
+										</select>
+
+										<span class="fa fa-angle-down"></span>
+									</div>
+								</div>
+
+								<div class="input select">
+									<label>Capítulo</label>
+									
+									<div class="select-box">
+										<select name="capitulo" required>
+											<option value="">Seleccione</option>
+											<option value="1">Todos</option>
+										</select>
+
+										<span class="fa fa-angle-down"></span>
+									</div>
+								</div>
+
+								<div class="input radio-choice">
+									<label>Exportar</label>
+									
+									<div class="box-column">
+										<div class="input radio">
+											<input type="radio" name="exporto" id="tipo1">
+											<label for="tipo1">Excel <span></span></label>
+										</div>
+
+										<div class="input radio">
+											<input type="radio" name="exporto" id="tipo2">
+											<label for="tipo2">CSV <span></span></label>
+										</div>
+									</div>
+								</div>
+
+								<div class="input radio-choice radio2">
+									<label>Paises</label>
+									
+									<div class="input radio">
+										<input type="radio" name="detalle" id="tipo3">
+										<label for="tipo3">Ver Detalle <span></span></label>
+									</div>
+								</div>
+
+								<div class="buttons">
+									<a href="/#/exportar" >Exportar</a>
+
+									<input type="submit" value="Consultar">
+								</div>
+							</div>
+						</fieldset>
+
+						<p class="error"></p>
+			    </form>
+			  </div>
+			</div>
+		</div>
+
+		<div class="bottom-content">
+			<div class="indicadores">
+				<p><strong>INDICADORES ECONOMICOS</strong>    <span>UF: 25.350,09</span>   -    <span>Dólar Obs: $715,8</span>   -  <span>Euro: $710,5</span></p>
+			</div>
+
+			<div class="buscador">
+        <input type="text" class="in" placeholder="Que desea buscar..."/>
+        <input name="" class="bt" type="button" value="" />
+			</div>
+		</div>
+	</div>
+</div>
